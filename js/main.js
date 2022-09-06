7df5ddb93982497ee7a0cf43a99cd7924e1e498a
Valeria Leza Sanchez <valerialeza.s@gmail.com>
2022-09-06 12:03:39 -0500

    Ejercicio 2 Resuelto

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Ejercicio 1 resuelto
+
+let numeros = [3, 5, 7, 1, 6];
+/* console.log(numeros);
+numeros.sort(function(a, b) {return a - b});
+console.log(numeros[numeros.length-1]); */
+let menor = "";
+for (let cont = 0; cont < numeros.length; cont++) {
+    
+   if (numeros[cont]<numeros[cont+1]) {
+    menor=console.log(numeros[cont]);
+   }
+}
