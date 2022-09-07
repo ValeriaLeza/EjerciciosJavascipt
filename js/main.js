133eb8769426d36008ec5d2f0540d9d45b04c64b
Valeria Leza Sanchez <valerialeza.s@gmail.com>
2022-09-07 15:25:09 -0500

    Ejercicio 5.2 resuelto

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let btnV = document.getElementById("btnValidar");
+
+btnV.addEventListener("click",function(e){
+    e.preventDefault();
+    let campo1= parseInt(document.getElementById("n1").value);
+    let campo2= parseInt(document.getElementById("n2").value);
+    let campo3= parseInt(document.getElementById("n3").value);
+    let res = document.getElementById("alertResultado");
+
+    console.log(campo1);
+    console.log(campo2);
+    console.log(campo3);
+
+    if (campo1>100 || campo1==0){
+        document.getElementById("n1").style.borderColor = "#ed3b3b";
+    } else {
+        document.getElementById("n1").style.borderColor = "#00FF00";
+    }
+    if (campo2>100 || campo2==0){
+        document.getElementById("n2").style.borderColor = "#ed3b3b";
+    } else {
+        document.getElementById("n2").style.borderColor = "#00FF00";
+    }
+    if (campo3>100 || campo3==0){
+        document.getElementById("n3").style.borderColor = "#ed3b3b";
+    } else {
+        document.getElementById("n3").style.borderColor = "#00FF00";
+    }
+
+    if (campo1<campo2) {
+        menor=campo1
+    } else {
+        menor=campo2
+    }
+    if (campo3<menor) {
+        menor=campo3
+    }
+
+if (menor>100) {
+    res.innerHTML = "Los numeros no estan dentro del rango";
+} else{
+    res.innerHTML = "El numero menor es: " + menor;
+}
+    
+});
